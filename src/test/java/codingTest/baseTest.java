b488dd29c3f0074a7a9900d2cb345646667688b4
장성우 <simplejs@nasmedia.co.kr>
2023-08-07 17:33:54 +0900

    오타수정

--- src/test/java/codingTest/baseTest.java
+++ src/test/java/codingTest/baseTest.java
@@ -18,7 +18,7 @@ public class baseTest { //클래스 명은 본인 클래스 이름에 맞게 변
         int a = 1;
         int b = 1;
 
-        MatcherAssert.assertThat("a와 b는 값다", a==b, is(true));
+        MatcherAssert.assertThat("a와 b는 같다", a==b, is(true));
     }
 }
 /*
